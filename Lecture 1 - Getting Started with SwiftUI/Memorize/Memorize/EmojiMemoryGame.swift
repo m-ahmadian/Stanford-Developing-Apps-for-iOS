435813c10d14f21be404b11e9ea5c492c574ce2a
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2022-03-07 23:41:43 -0500

    Lecture 5 - Properties Layout @ViewBuilder

--- Lecture 1 - Getting Started with SwiftUI/Memorize/Memorize/EmojiMemoryGame.swift
+++ Lecture 1 - Getting Started with SwiftUI/Memorize/Memorize/EmojiMemoryGame.swift
@@ -8,23 +8,25 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    static let emojis = ["🚗","🚌","🚑","🚜","🛴","🚲","🚔","🛺","🚘","🚃","🚄","🚠","🚅","✈️","🚀","🛸","🚁","🛶","🚤","🚢","🛳","🚓","🚛","🛵"]
+    typealias Card = MemoryGame<String>.Card
 
-    static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 3) { pairIndex in
+    private static let emojis = ["🚗","🚌","🚑","🚜","🛴","🚲","🚔","🛺","🚘","🚃","🚄","🚠","🚅","✈️","🚀","🛸","🚁","🛶","🚤","🚢","🛳","🚓","🚛","🛵"]
+
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 6) { pairIndex in
                 emojis[pairIndex]
             }
     }
 
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model = createMemoryGame()
 
-    var cards: Array<MemoryGame<String>.Card> {
+    var cards: Array<Card> {
         model.cards
     }
 
     // MARK: - Intent(s)
 
-    func choose(_ card: MemoryGame<String>.Card) {
+    func choose(_ card: Card) {
         model.choose(card)
     }
 }
